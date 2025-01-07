71559617d0417eaa62360ebbe634981717cbd998
Belrayy <111245340+Belrayy@users.noreply.github.com>
2025-01-07 10:07:53 +0000

    js2

--- Js2/script.js
+++ Js2/script.js
@@ -1,6 +1,6 @@
 /*Name this external file gallery.js*/
 
-function upDate(previewPic){
+function upDate(previewPic) {
     /* In this function you should 
        1) change the url for the background image of the div with the id = "image" 
        to the source file of the preview image
@@ -8,16 +8,21 @@ function upDate(previewPic){
        2) Change the text  of the div with the id = "image" 
        to the alt text of the preview image 
        */
-     
-       }
+       let imageDiv = document.getElementById("image");
+       imageDiv.style.backgroundImage = `url(${previewPic.src})`;
+       imageDiv.innerHTML = previewPic.alt
+}
+
+function unDo() {
+    /* In this function you should 
+   1) Update the url for the background image of the div with the id = "image" 
+   back to the orginal-image.  You can use the css code to see what that original URL was
    
-       function unDo(){
-        /* In this function you should 
-       1) Update the url for the background image of the div with the id = "image" 
-       back to the orginal-image.  You can use the css code to see what that original URL was
-       
-       2) Change the text  of the div with the id = "image" 
-       back to the original text.  You can use the html code to see what that original text was
-       */
-           
-       }
+   2) Change the text  of the div with the id = "image" 
+   back to the original text.  You can use the html code to see what that original text was
+   */
+   let imageDiv = document.getElementById("image");
+   imageDiv.style.backgroundImage = "url('')";
+   imageDiv.innerHTML = "Hover over an image below to display here.";
+
+}
