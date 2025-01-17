be42d396db68593ab24e3c6d2e77c51695d2114b
Belrayy <111245340+Belrayy@users.noreply.github.com>
2025-01-17 11:28:13 +0000

    c

--- Cpp/chimie.cpp
+++ Cpp/chimie.cpp
@@ -6,54 +6,46 @@ using namespace std;
 class Flacon
 {
 private:
-  string Nom;
-  double Volume;
+  string nom;
+  double volume;
   double pH;
 
 public:
   /*****************************************************
     Compléter le code à partir d'ici
   *******************************************************/
-  string getNom(void) const
+public:
+  string getNom(void) const {return nom;}
+  double getVolume(void) const {return volume;}
+  double getPH(void) const {return pH;}
+  
+  Flacon(string n, double v, double p) : nom(n), volume(v), pH(p) {}
+  
+  ostream& etiquette(ostream& sortie) const
   {
-    return Nom;
-  }
-  double getVolume(void) const
-  {
-    return Volume;
-  }
-  double getPH(void) const
-  {
-    return pH;
-  }
-
-  Flacon(string n, double v, double p) : Nom(n), Volume(v), pH(p) {}
-
-  ostream &etiquette(ostream &sortie) const
-  {
-    sortie << Nom << " : " << Volume << " ml, pH " << pH << endl;
-    return sortie;
+	sortie << nom << " : " << volume << " ml, pH " << pH ;
+	return sortie;
   }
 };
-ostream &operator<<(ostream &sortie, Flacon const &z)
+
+ostream& operator<<(ostream& sortie, Flacon const& z)
 {
-  return z.etiquette(sortie);
+	return z.etiquette(sortie);
 }
 
-const Flacon operator+(Flacon const &flac1, Flacon const &flac2)
+const Flacon operator+(Flacon const& z1, Flacon const& z2) 
 {
-  string nom = flac1.getNom() + " + " + flac2.getNom();
-  double vol = flac1.getVolume() + flac2.getVolume();
-  double pH = -log10((flac1.getVolume() * pow(10, -flac1.getPH()) + flac2.getVolume() * pow(10, -flac2.getPH())) / (flac1.getVolume() + flac2.getVolume()));
-  Flacon flac(nom, vol, pH);
-  return flac;
+  string n = z1.getNom() + " + " + z2.getNom();
+  double v = z1.getVolume() + z2.getVolume();
+  double p = -log10((z1.getVolume()*pow(10,-z1.getPH()) + z2.getVolume()*pow(10,-z2.getPH()))/(z1.getVolume()+z2.getVolume()));
+  Flacon flac(n, v, p);
+  return flac;	
 }
-
 /*******************************************
  * Ne rien modifier après cette ligne.
  *******************************************/
 
-void afficher_melange(Flacon const &f1, Flacon const &f2)
+void afficher_melange(Flacon const& f1, Flacon const& f2)
 {
   cout << "Si je mélange " << endl;
   cout << "\t\"" << f1 << "\"" << endl;
@@ -67,10 +59,11 @@ int main()
 {
   Flacon flacon1("Eau", 600.0, 7.0);
   Flacon flacon2("Acide chlorhydrique", 500.0, 2.0);
-  Flacon flacon3("Acide perchlorique", 800.0, 1.5);
+  Flacon flacon3("Acide perchlorique",  800.0, 1.5);
 
   afficher_melange(flacon1, flacon2);
   afficher_melange(flacon2, flacon3);
 
   return 0;
+
 }
--- Cpp/construction.cpp
+++ Cpp/construction.cpp
@@ -4,168 +4,158 @@
 using namespace std;
 
 // Pour simplifier
-typedef string Forme;
-typedef string Couleur;
+typedef string Forme   ;
+typedef string Couleur ;
 
 class Brique
 {
 private:
-  Forme forme;
-  Couleur couleur;
+  Forme   forme   ;
+  Couleur couleur ;
 
 public:
   /*****************************************************
     Compléter le code à partir d'ici
   *******************************************************/
-  Brique(Forme forme, Couleur couleur)
-      : forme(forme), couleur(couleur) {}
 
-  ostream &afficher(ostream &sortie) const
-  {
-    if (couleur != "")
+  //Constructor
+    Brique(Forme forme, Couleur couleur)
+    : forme(forme), couleur(couleur) {}
+
+    ostream& afficher(ostream& sortie) const 
     {
-      sortie << "(" << forme << ", " << couleur << ")";
+        if (couleur != ""){sortie << "(" << forme << ", " << couleur << ")"; }
+        else {sortie << forme;}
+        return sortie;
     }
-    else
-    {
-      sortie << forme;
-    }
-    return sortie;
-  }
 };
 
-ostream &operator<<(ostream &sortie, Brique const &B) { return B.afficher(sortie); }
+ostream& operator<<(ostream& sortie, Brique const & B) {  return B.afficher(sortie);}
 
 typedef vector<Brique> Line;
 typedef vector<Line> Surface;
 typedef vector<Surface> Volume;
 
+
 class Construction
 {
 private:
-  Volume contenu;
-  friend class Grader;
-
+    Volume contenu;
+    friend class Grader;
 public:
-  Construction(Brique B)
-      : contenu(Volume(1, Surface(1, Line(1, B)))) {}
+    Construction(Brique B)
+    :contenu(Volume(1,Surface(1,Line(1,B))) ){}
 
-  int height() const
-  {
-    return contenu.size();
-  }
-
-  int depth() const
-  {
-    /**
-    int depth=0;
-    for (int i=0; i<= height(); i++)
+    int height() const
     {
-       if (contenu[i].size() > depth)
-       {
-           depth = contenu[i].size();
-       }
+        return contenu.size();
     }
-    */
-    int depth = contenu[0].size();
-    return depth;
-  }
 
-  ostream &afficher(ostream &sortie) const
-  {
-    for (int i = contenu.size() - 1; i >= 0; i--)
+    int depth() const
     {
-      sortie << "Couche " << i << " :" << endl;
-      for (int j = 0; j < contenu[i].size(); j++)
-      {
-        for (int k = 0; k < contenu[i][j].size(); k++)
+        /**
+        int depth=0;
+        for (int i=0; i<= height(); i++)
         {
-          sortie << contenu[i][j][k];
+           if (contenu[i].size() > depth)
+           {
+               depth = contenu[i].size();
+           }
         }
-        sortie << endl;
-      }
+        */
+        int depth = contenu[0].size();
+        return depth;
     }
-    return sortie;
-  }
-
-  Construction &operator^=(Construction const &C)
-  {
-    for (int i = 0; i < C.contenu.size(); i++)
+  
+    ostream& afficher(ostream& sortie) const 
     {
-      contenu.push_back(C.contenu[i]);
-    }
-    return *this;
-  }
-
-  Construction &operator-=(Construction const &C)
-  {
-    if (height() <= C.height())
-    {
-      for (int i = 0; i < contenu.size(); i++)
-      {
-        for (int j = 0; j < C.contenu[i].size(); j++)
+        for (int i=contenu.size() -1; i >= 0 ; i--)
         {
-          contenu[i].push_back(C.contenu[i][j]);
+            sortie << "Couche " << i << " :" << endl;
+            for (int j=0; j < contenu[i].size() ; j++ )
+            {
+                for (int k=0; k < contenu[i][j].size() ; k++)
+                {
+                    sortie<<contenu[i][j][k];
+                }
+                sortie << endl ;
+            }
         }
-      }
+        return sortie;
     }
-    return *this;
-  }
 
-  Construction &operator+=(Construction const &C)
-  {
-    if (height() <= C.height() && depth() <= C.depth())
+    Construction& operator^=(Construction const&  C)
     {
-      for (int i = 0; i < contenu.size(); i++)
-      {
-        for (int j = 0; j < contenu[i].size(); j++)
+        for (int i=0; i < C.contenu.size(); i++)
         {
-          for (int k = 0; k < C.contenu[i][j].size(); k++)
-          {
-            contenu[i][j].push_back(C.contenu[i][j][k]);
-          }
+            contenu.push_back(C.contenu[i]);
         }
-      }
+        return *this;
     }
-    return *this;
-  }
+
+    Construction& operator-=(Construction const&  C)
+    {
+        if (height() <= C.height() )
+        {
+            for (int i=0; i < contenu.size(); i++)
+            {
+                for (int j=0; j < C.contenu[i].size(); j++)
+                {
+                    contenu[i].push_back(C.contenu[i][j]);
+                }
+            }
+        }
+        return *this;
+    }
+
+    Construction& operator+=(Construction const&  C)
+    {
+        if (height() <= C.height() && depth() <= C.depth() )
+        {
+            for (int i=0; i < contenu.size(); i++)
+            {
+                for (int j=0; j < contenu[i].size(); j++)
+                {
+                    for (int k=0; k < C.contenu[i][j].size(); k++)
+                    {
+                        contenu[i][j].push_back(C.contenu[i][j][k]);
+                    }
+                } 
+            }
+        }
+        return *this;
+    }
 };
 
-ostream &operator<<(ostream &sortie, Construction const &C) { return C.afficher(sortie); }
 
-const Construction operator^(Construction C1, Construction const &C2) { return C1 ^= C2; }
-const Construction operator-(Construction C1, Construction const &C2) { return C1 -= C2; }
-const Construction operator+(Construction C1, Construction const &C2) { return C1 += C2; }
+ostream& operator<<(ostream& sortie, Construction const & C) {  return C.afficher(sortie);}
 
-const Construction operator*(unsigned int n, Construction const &a)
+const Construction operator^(Construction C1, Construction const& C2){ return C1^=C2;}
+const Construction operator-(Construction C1, Construction const& C2){ return C1-=C2;}
+const Construction operator+(Construction C1, Construction const& C2){ return C1+=C2;}
+
+
+const Construction operator*(unsigned int n, Construction const& a)
 {
-  Construction aux = a;
-  for (unsigned int i = 0; i < n - 1; i++)
-  {
-    aux += a;
-  }
-  return aux;
+    Construction aux = a;
+    for (unsigned int i=0; i<n-1; i++ ){aux+=a; }
+    return aux;
 }
 
-const Construction operator/(unsigned int n, Construction const &a)
+const Construction operator/(unsigned int n, Construction const& a)
 {
-  Construction aux = a;
-  for (unsigned int i = 0; i < n - 1; i++)
-  {
-    aux ^= a;
-  }
-  return aux;
+    Construction aux = a;
+    for (unsigned int i=0; i<n-1; i++ ){aux^=a; }
+    return aux;
 }
 
-const Construction operator%(unsigned int n, Construction const &a)
+const Construction operator%(unsigned int n, Construction const& a)
 {
-  Construction aux = a;
-  for (unsigned int i = 0; i < n - 1; i++)
-  {
-    aux -= a;
-  }
-  return aux;
+    Construction aux = a;
+    for (unsigned int i=0; i<n-1; i++ ){aux-=a; }
+    return aux;
 }
+
 /*******************************************
  * Ne rien modifier après cette ligne.
  *******************************************/
@@ -176,18 +166,18 @@ int main()
   Brique toitD("obliqueD", "rouge");
   Brique toitG("obliqueG", "rouge");
   Brique toitM(" pleine ", "rouge");
-  Brique mur(" pleine ", "blanc");
-  Brique vide("                 ", "");
+  Brique mur  (" pleine ", "blanc");
+  Brique vide ("                 ", "");
 
   unsigned int largeur(4);
   unsigned int profondeur(3);
   unsigned int hauteur(3); // sans le toit
 
   // on construit les murs
-  Construction maison(hauteur / (profondeur % (largeur * mur)));
+  Construction maison( hauteur / ( profondeur % (largeur * mur) ) );
 
   // on construit le toit
-  Construction toit(profondeur % (toitG + 2 * toitM + toitD));
+  Construction toit(profondeur % ( toitG + 2*toitM + toitD ));
   toit ^= profondeur % (vide + toitG + toitD);
 
   // on pose le toit sur les murs
