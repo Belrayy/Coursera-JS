807afc86bdc73429b347646c2ec8dbd505cfbf05
Belrayy <111245340+Belrayy@users.noreply.github.com>
2025-03-05 11:23:27 +0000

    Python 3

--- Python/sum.py
+++ Python/sum.py
@@ -9,6 +9,6 @@ def sum_file(filename):
     return total
 
 #filen = r"C:\Users\Belrayy\Documents\EMSI\Coursera\Python\regex_sum_42.txt" the answer is 445833
-filen = r"C:\Users\Belrayy\Documents\EMSI\Coursera\Python\regex_sum_2189644.txt"
+filen = r"C:\Users\Belrayy\Documents\EMSI\Coursera\Python\regex_sum_.txt"
 result = sum_file(filen)
 print(f"The sum of all numbers in the file is: {result}")
